3b9839ab83c18cc89c8fee0f2901a6a026871cb0
Tasank <apanasovich04@mail.ru>
2024-10-25 21:33:12 +0500

    тест

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('contacts').addEventListener('click', function() {
+    const menu = document.querySelector('.contact-menu');
+    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+});
